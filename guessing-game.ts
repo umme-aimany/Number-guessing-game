e42c26444b8c51bba6d076ae901f3770408d22ee
Umme Aiman <khanummeaiman91@gmail.com>
2024-06-28 14:56:08 +0500

    First-commit

---
+++ guessing-game.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber=Math.floor(Math.random()* 10 + 1) //////+1 isliya kiya ha taka 0 na ai ///////////or 10 means ya 1 sa 10 tak ka number generate karai ga
+const game=await inquirer.prompt([
+    {
+    name: "userGuessnumber",
+    type:"number",
+    message:"Please guess a number between 1-10",
+    }
+
+
+]);
+console.log(game);
+if(game.userGuessnumber===randomNumber)
+    {
+    console.log("Congratulations!you guess right number")
+
+}
+else{
+    console.log("OOPS!you guess wrong number")
+}
